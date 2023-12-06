6d7145e8ed6726c2d4244bc7bd4007ec39e04940
AleRat87 <93096019+AleRat87@users.noreply.github.com>
2023-12-06 12:46:39 +0200

    Update with character and line check p9a.sh

--- p9a.sh
+++ p9a.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]
-then
-    echo "numar incorect de argumente"
+    then echo "Numar insuficient de argumente"
     exit 1
 fi
-//Se adauga conditiile pentru propozitii cu grep
+
 nr_linii=0
+character=$1
 
-while read linie
+while IFS= read -r linie
 do
-    nr_linii=$(($nr_linii+1));
-
+    expr="^[A-Z][A-Za-z0-9[:space:],.!?]*[?!.]$"
+    if echo "$linie" | grep -q "$character";
+    then 
+        if echo "$linie" | grep -qE "$expr" && ! echo "$linie" | grep -q ", si";
+            then nr_linii=$(($nr_linii+1));
+        fi
+    fi
 done
 
 echo "$nr_linii"
